056f67c137c2b67f253ef809291f7e84c74e224c
chris <cching@gmail.com>
2016-03-02 23:13:44 +0000

    program moves an @ symbol around the screen baced on keyboard input

--- splatRunner/Program.cs
+++ splatRunner/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace splatRunner
+namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string playerSymbol = "@";
+        static int cursorX = Console.WindowHeight / 2;
+        static int cursorY = Console.WindowWidth / 2;
+
+        static bool shouldContinue = true;
+
+        static readonly Dictionary<ConsoleKey, Action> KeyActions = new Dictionary<ConsoleKey, Action>
         {
+            [ConsoleKey.LeftArrow] = () => cursorY = cursorY - 1,
+            [ConsoleKey.RightArrow] = () => cursorY = cursorY + 1,
+            [ConsoleKey.UpArrow] = () => cursorX = cursorX - 1,
+            [ConsoleKey.DownArrow] = () => cursorX = cursorX + 1,
+            [ConsoleKey.Escape] = () => shouldContinue = false,
+        };
+
+        private static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            while (shouldContinue)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(cursorY, cursorX);
+                Console.Write(playerSymbol);
+
+                var move = Console.ReadKey();
+
+                if (KeyActions.ContainsKey(move.Key))
+                {
+                    KeyActions[move.Key].Invoke();
+                }
+            }
         }
     }
 }
